e2ff59aa98fd6e694e3301b71ae850c9a35f70b8
KAVlex <kavlex90@gmail.com>
2017-05-18 00:59:27 +0400

    Пункты 2-3 домашнего задания

--- app/src/PokemonsService/PokemonsService.js
+++ app/src/PokemonsService/PokemonsService.js
@@ -1,44 +1,38 @@
 angular
     .module('PokemonApp')
-    .factory('PokemonsService', function($http) {
+    .factory('PokemonsService', function ($http) {
+
+            $http.defaults.headers.common = {
+                "application-id": "B9CE46A9-7E97-DFA1-FF39-3AFE25BBEE00",
+                "secret-key": "29C0795C-7BC9-58A1-FF8F-1620D9A62C00"
+            };
 
             return {
 
-                getPokemons: function() {
+                getPokemons: function () {
                     return $http.get('http://pokeapi.co/api/v2/pokemon/?limit=10');
                 },
 
-                getPokemon: function(pokemonId) {
+                getPokemon: function (pokemonId) {
                     return $http.get('http://pokeapi.co/api/v2/pokemon/' + pokemonId);
                 },
 
-                createPokemon: function(pokemonData) {
+                createPokemon: function (pokemonData) {
                     return $http({
                         method: 'POST',
                         url: 'https://api.backendless.com/v1/data/pokemon',
-                        headers: {
-                            "application-id": "4B730C92-F81E-236B-FFF0-6651FE882800",
-                            "secret-key": "CB6DE86C-6069-86C4-FF1C-9049D5AC9400"
-
-                        },
                         data: pokemonData
                     });
                 },
 
-                deletePokemon: function(pokemonId) {
+                deletePokemon: function (pokemonId) {
                     return $http({
                         method: 'DELETE',
                         url: 'https://api.backendless.com/v1/data/pokemon/' + pokemonId,
-                        headers: {
-                            "application-id": "4B730C92-F81E-236B-FFF0-6651FE882800",
-                            "secret-key": "CB6DE86C-6069-86C4-FF1C-9049D5AC9400"
-
-                        }
                     });
                 }
 
             }
 
         }
-
     );
