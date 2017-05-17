a565fd887a8d567edc1b141a3487def17bf5e43e
KAVlex <kavlex90@gmail.com>
2017-05-18 00:00:26 +0400

    лоадеры в список покемонов и ягод

--- app/src/PokemonList/PokemonListCtrl.js
+++ app/src/PokemonList/PokemonListCtrl.js
@@ -2,22 +2,14 @@
 
 pokemonApp.controller('PokemonListCtrl', function($scope, PokemonsService, BerriesService) {
 
-    // PokemonsService.getPokemons().then(function(response) {
-    //     $scope.pokemons = response.data.results;
-    // });
-    //
-    // BerriesService.getBerries().then(function(response) {
-    //     $scope.berries = response.data.results;
-    // });
+    $scope.status = 'loading';
 
     PokemonsService.getPokemons().then(function(response) {
         $scope.pokemons = response.data.results;
-
         return BerriesService.getBerries()
     }).then(function(response) {
         $scope.berries = response.data.results;
+        $scope.status = 'ready';
     });
-
-
 
 });
